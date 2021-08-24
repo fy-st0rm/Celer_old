3448d510f2d3617092100b15b8e5fabf9cabcf66
fy-st0rm <83293237+fy-st0rm@users.noreply.github.com>
2021-08-24 15:20:36 +0545

    fixed some UI

---
+++ celer_ui.py
@@ -0,0 +1,123 @@
+import tkinter as tk
+import tkinter.font
+from tkinter import messagebox
+from tkinter import ttk
+from tkinter import messagebox
+from celer_ui_main import*
+
+
+class log_ui:
+	def __init__(self, network):
+		self.window = tk.Tk(className = 'celer')
+		self.network = network
+
+
+		#Importing stuff
+		self.bg_img = tk.PhotoImage(file = 'Ui/Images/bg.png')
+		self.button_nopressed = tk.PhotoImage(file = 'Ui/Images/notpressed.png')
+		self.button_pressed = tk.PhotoImage( file = 'Ui/Images/pressed.png')
+		self.button_nopressed_up = tk.PhotoImage( file = 'Ui/Images/notpressed_up.png')
+		self.button_pressed_up = tk.PhotoImage( file = 'Ui/Images/pressed_up.png')
+
+		#background
+		self.bg = tk.Label(image = self.bg_img)
+
+		#Tabs Setup
+		self.notebook = ttk.Notebook(self.window,style="TNotebook")
+		self.tab1 = tk.Frame(self.notebook, width = 350, height = 500, bg = 'white')
+		self.tab2 = tk.Frame(self.notebook, width = 350, height = 500, bg = 'white')
+
+		#Defining General font
+		self.font = tk.font.Font( family = 'Bahnschrift Light', size = 20)
+
+		#Wigets in tab1
+		self.title_signin = tk.Label(self.tab1,text = 'celer\nSign in Window', font = self.font, fg = '#6e6e6e', bg ='white')
+		self.title_username_signin = tk.Label(self.tab1,text = 'Username:', font = self.font, fg = '#6e6e6e', bg ='white')
+		self.username_signin = tk.Entry(self.tab1, bg = '#d6d6d6', width = 15, borderwidth = 0, font = self.font)
+		self.title_password_signin = tk.Label(self.tab1,text = 'Password:', font = self.font, fg = '#6e6e6e', bg ='white')
+		self.password_signin = tk.Entry(self.tab1, bg = '#d6d6d6', width = 15, borderwidth = 0, font = self.font, show = '*')
+		self.button_singnin = tk.Button(self.tab1, text = 'Signin!', command = self.getdataSignin, image = self.button_nopressed, bg = 'white', borderwidth = 0, relief='sunken', highlightthickness=0, bd=0)
+
+		#Wigets in tab2
+		self.title_signup = tk.Label(self.tab2,text = 'celer\nSign up Window', font = self.font, fg = '#6e6e6e', bg ='white')
+		self.title_username_signup = tk.Label(self.tab2,text = 'Username:', font = self.font, fg = '#6e6e6e', bg ='white')
+		self.username_signup = tk.Entry(self.tab2, bg = '#d6d6d6', width = 20, borderwidth = 0, font = self.font)
+		self.title_password_signup = tk.Label(self.tab2,text = 'Password:', font = self.font, fg = '#6e6e6e', bg ='white')
+		self.password_signup = tk.Entry(self.tab2, bg = '#d6d6d6', width = 20, borderwidth = 0, font = self.font, show = '*')
+		self.button_singnup = tk.Button(self.tab2, text = 'Signup!', command = self.getdataSignup, image = self.button_nopressed_up, bg = 'white', borderwidth = 0, relief='sunken', highlightthickness=0, bd=0)	
+
+
+
+	def drawUI(self):
+		self.bg.place(x=-1, y=0)
+		self.notebook.place(x=490,y=30)#Drawing notebook
+
+		#Drawing widgets in tab1
+		self.title_signin.place(x=80,y=0)
+		self.title_username_signin.place(x = 30, y = 160)
+		self.username_signin.place(x=30, y=200, height = 40)
+		self.title_password_signin.place(x = 30, y = 260)
+		self.password_signin.place(x = 30, y = 300, height = 40)
+		self.button_singnin.place(x=40, y= 400)
+
+		#Drawing widgets in tab2
+		self.title_signup.place(x=80,y=0)
+		self.title_username_signup.place(x=30,y=160)
+		self.username_signup.place(x=30,y=200, height = 40)
+		self.title_password_signup.place(x=30,y=260)
+		self.password_signup.place(x=30,y=300,height=40)
+		self.button_singnup.place(x=40,y=400)
+
+		#Tab drawing stuff
+		self.tab1.place(x =0,y=0, anchor = 'center')
+		self.tab2.place(x =0,y=0, anchor = 'center')
+		self.notebook.add(self.tab1, text = 'Signin')
+		self.notebook.add(self.tab2, text = 'Signup')
+
+	def getdataSignin(self):
+		self.button_singnin.config(image = self.button_pressed)
+		username = self.username_signin.get()#Variable for username of Tab1
+		password = self.password_signin.get()#Variable for password of Tab1
+
+		token = "[LOGIN]"
+		info = f"{token} username:{username} password:{password}"
+		self.network.send(info)
+
+		# Reciving the reply from the server
+		reply = self.network.recv()
+		
+		if reply == "[ACCEPTED]":
+			self.window.destroy()
+			self.mainUi = main_ui(username, self.network)
+
+			self.mainUi.startUI()
+			
+		elif reply == "[REJECTED]":
+			tk.messagebox.showerror("error", "username or password is wrong!")
+
+	def getdataSignup(self):
+		self.button_singnup.config(image = self.button_pressed_up)
+		username = self.username_signup.get()#Variable for username of Tab2
+		password = self.password_signup.get()	#Variable for password of Tab2
+		
+		if len(password) != 8:
+			tk.messagebox.showerror("error", "password limit is 8 characters!")		
+		else:
+			token = "[SIGNUP]"
+			info = f"{token} username:{username} password:{password}"
+			self.network.send(info)
+			# Reciving the reply from the server
+			reply = self.network.recv()
+			if reply == "[ACCEPTED]":
+				self.window.destroy()
+				self.mainUi = main_ui(username, self.network)
+				self.mainUi.startUI()
+			elif reply == "[REJECTED]":
+				tk.messagebox.showerror("error", "username already exsists!")	
+
+	def winUI(self):
+		#Window setup
+		self.window.geometry('900x600')	
+		self.window.resizable(False, False)
+		self.window.mainloop()
+   
---
+++ celer_ui_main.py
@@ -0,0 +1,216 @@
+import tkinter as tk
+import tkinter.font
+from tkinter import messagebox
+from tkinter import ttk
+from tkinter import messagebox
+import platform
+import threading
+import socket
+
+from encrypt import *
+
+
+class main_ui:
+	def __init__(self, user, network):
+		self.window = tk.Tk(className = 'celer')
+
+		# Networking stuff
+		self.network = network
+		self.running = True
+		self.user = user
+		self.key = ""
+
+		#Importing stuff
+		self.add_server_button_normal = tk.PhotoImage(file = 'Ui/Images/server_add_normal.png')
+		self.join_server_button_normal = tk.PhotoImage(file = 'Ui/Images/server_join_normal.png')
+		self.ok_button = tk.PhotoImage(file = 'Ui/Images/ok.png')
+
+		#Defining General font
+		self.font = tk.font.Font( family = 'Bahnschrift Light', size = 20)
+		self.font2 = tk.font.Font( family = 'Bahnschrift Light', size = 10)
+
+		#Defining stuff
+		self.chatEntry = tk.Entry(self.window, width = 60, font = self.font, borderwidth = 0, bg = '#40444b', fg= 'white') #Chat text box!
+		self.chatDisplay = tk.Text(self.window, width = 60, font = self.font, bg = '#36393f', borderwidth = 0, relief='solid', highlightthickness=0, fg = 'white')
+		self.serverList = tk.Listbox(self.window, width = 0, height = 100,font = self.font, bg = '#2e3137', fg = 'white', selectforeground='Black', selectbackground = 'white', activestyle='none', borderwidth = 0, relief='solid', highlightthickness=0)
+		self.serverSelect = self.serverList.curselection()
+		self.serverCreatebutton = tk.Button(command = self.createServer, image = self.add_server_button_normal, borderwidth = 0, relief='sunken', bg = '#212325', activebackground="#212325", highlightthickness=0, bd=0)
+		self.serverJoinbutton = tk.Button(command = self.joinServer, image = self.join_server_button_normal, borderwidth = 0, relief='sunken', bg = '#212325', activebackground="#212325", highlightthickness=0, bd=0)
+
+		
+		#Checks if enter is pressed
+		self.chatEntry.bind('<Return>', self.chatEntryDataGet)
+
+		#Checks if mouse 1 is pressed in the server list
+		self.serverList.bind('<1>',self.connectServerselected)
+
+		#Server list number temporary variable
+		self.varNum = 0
+
+		#Gets the data of selected item from the list
+		self.selectionServer = self.serverList.curselection()
+
+		#Checks for the type of the os for fullscreen
+		if platform.system() == "Linux":
+			self.window.attributes('-zoomed', True)
+		elif platform.system() == "Windows":
+			self.window.state("zoomed")		
+
+
+
+	def __chat_ui(self):	
+		self.chatEntry.pack(side = 'bottom')
+		self.chatDisplay.pack(side = 'top')
+		
+		self.chatDisplay.config(state = "normal")
+		self.chatDisplay.delete(1.0, "end")
+		self.chatDisplay.config(state = "disabled")
+
+	def winUI(self):
+		self.window.geometry('940x500')
+		self.window.wm_minsize(940, 500) #User can resize up to 940,500
+		self.window.configure(bg='#212325' )
+		self.window.mainloop()	
+
+	def chatEntryDataGet(self,event):
+		self.chatEntryData = self.chatEntry.get() #Puts the data in the variable
+		self.chatEntry.delete(0, 'end')#Deletes the data written in entry
+		
+		# Sending the msg to server
+		token = "[MSG]"
+		enc_data = encrypt(f"[{self.user}]: {self.chatEntryData}\n", self.key)
+		data = f"{token} {enc_data}"
+		self.network.send(data)
+
+	def __receiver(self):
+		while self.running:
+			recv_info = self.network.recv()
+			tokens = recv_info.split(" ")
+
+			if tokens[0] == "[DISCONNECT]":
+				self.running = False
+
+			elif tokens[0] == "[SERVER]":
+				tokens.pop(0)
+				self.serverList.delete(0,'end')#Deletes the old list to remove repeating list
+				for i in tokens:
+					key = i.split(":")[0]
+					name = i.split(":")[1]
+					self.varNum+=1 #Server's position in the list
+					self.serverNames = key+":"+name 
+					self.serverList.insert(self.varNum,self.serverNames)#Inserts server list
+
+			# When server creation failed
+			elif tokens[0] == "[REJECTED]":
+				self.key = self.__create_sv(self.svName)	
+				self.__join_sv(self.key)
+			elif tokens[0] == "[ACCEPTED]":
+				self.__join_sv(self.key)
+
+			# When it catches a msg
+			elif tokens[0] == "[MSG]":
+				if len(tokens) > 1:
+					self.chatDisplay.config(state = "normal")
+
+					tokens.pop(0)
+					enc_msg = " ".join(tokens)
+					dec_msg = decrypt(enc_msg, self.key)
+					self.chatDisplay.insert("end", dec_msg)
+				
+					self.chatDisplay.config(state = "disabled")
+
+	def drawUI(self):
+		self.serverList.pack(side = 'left')
+		self.serverCreatebutton.pack(side = "top")
+		self.serverJoinbutton.pack(side = "top")
+
+
+	def startUI(self):
+		recv_thread = threading.Thread(target = self.__receiver)
+		recv_thread.start()
+
+		#Starts ui code
+		self.drawUI()
+		self.winUI()
+		
+
+	"""--------- Create server functions ----------"""
+	def createServer(self):
+		self.serverCreatewindow = tk.Toplevel(self.window) #Defining new window
+
+		#Defining stuff
+		self.serverName = tk.Entry(self.serverCreatewindow,width = 30, font = self.font2, borderwidth = 0, bg = '#40444b', fg = 'white' )
+		self.serverCreatelabel = tk.Label(self.serverCreatewindow, text = 'Enter the name of the server:', font = self.font2, bg = '#36393f', fg = 'white')
+		self.server_Createbutton = tk.Button(self.serverCreatewindow, command = self.revServername, image = self.ok_button, bg = '#36393f',relief='sunken', activebackground="#36393f", borderwidth = 0, highlightthickness=0, bd=0)
+
+		#Drawing stuff
+		self.serverCreatelabel.place(x=100,y=0)
+		self.serverName.place(x=100,y=30)
+		self.server_Createbutton.place( x = 180, y = 50)
+		
+		#Window property stuff
+		self.serverCreatewindow.resizable(False, False)
+		self.serverCreatewindow.title("Create Server")
+		self.serverCreatewindow.geometry("390x180")
+		self.serverCreatewindow.configure(bg = '#36393f')
+
+
+	def revServername(self):
+		self.svName = self.serverName.get()#Server Name data!
+		if self.svName:
+			self.key = self.__create_sv(self.svName)
+			self.serverCreatewindow.destroy()#Destroys server create window
+			tk.messagebox.showinfo("Information", "Server Created!") #Messages user that the server is created!
+
+	def __create_sv(self, name):	
+		# Sending the information about the creation of new server
+		token = "[NEW_SV]"
+		sv_key = generate_key()
+		info = f"{token} key:{sv_key} name:{name}"
+		self.network.send(info)
+		return sv_key
+
+	"""---------- Join server functions ----------"""
+	def joinServer(self):
+		self.serverJoinwindow = tk.Toplevel(self.window) #Defining new window
+
+		#Defining stuff
+		self.serverCode = tk.Entry(self.serverJoinwindow,width = 30, font = self.font2, borderwidth = 0, bg = '#40444b', fg = 'white'  )
+		self.serverJoinlabel = tk.Label(self.serverJoinwindow, text = 'Enter the code of the server:', font = self.font2, bg = '#36393f', fg = 'white')
+		self.serverJoinbutton = tk.Button(self.serverJoinwindow, command = self.revServercode, image = self.ok_button, bg = '#36393f',relief='sunken', activebackground="#36393f", borderwidth = 0, highlightthickness=0, bd=0)
+
+		#Drawing stuff
+		self.serverJoinlabel.place(x=100,y=0)
+		self.serverCode.place(x=100,y=30)
+		self.serverJoinbutton.place( x = 180, y = 50)
+		
+		#Window property stuff
+		self.serverJoinwindow.resizable(False, False)
+		self.serverJoinwindow.title("Join Server")
+		self.serverJoinwindow.geometry("390x180")
+		self.serverJoinwindow.configure(bg = '#36393f')	
+
+	def revServercode(self):
+		self.svCode = self.serverCode.get()#Server code data!	
+		if self.svCode:
+			self.__join_sv(self.svCode)	
+			self.serverJoinwindow.destroy()
+			tk.messagebox.showinfo("Information", "Server joined!")
+
+	def __join_sv(self, key):	
+		# Joining the sv
+		token = "[JOIN]"
+		info = f"{token} name:{self.user} key:{key}"
+		self.network.send(info)
+
+	def connectServerselected(self,event):
+		self.selectedServer = self.serverList.get('anchor')#Gets the data from the selected item
+		
+		# Telling the server that we selected a server
+		self.key = self.selectedServer.split(":")[0]
+		self.network.send("[SELECT] " + self.key)
+
+		self.chatEntry.pack_forget()
+		self.chatDisplay.pack_forget()
+
+		self.__chat_ui()
